fff1e65cd58933cf5703f10923c1529afa6168d3
Vinicius <masukawavini@hotmail.com>
2023-03-09 19:21:51 -0300

    Criando um banco de dados para armazenar as senhas

--- main.py
+++ main.py
@@ -1,5 +1,15 @@
 import random
 import string
+import sqlite3
+
+conexao = sqlite3.connect('senhas.db')
+
+conexao.execute('''
+        CREATE TABLE IF NOT EXISTS Senhas (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            senha TEXT NOT NULL
+        );
+''')
 
 def gerarSenha(comprimento):
     caracteres = string.ascii_letters + string.digits + string.punctuation
@@ -8,5 +18,8 @@ def gerarSenha(comprimento):
 
 comprimento = int(input("Digite o comprimento da senha desejada: "))
 senhaGerada = gerarSenha(comprimento)
+
+conexao.execute("INSERT INTO Senhas (senha) VALUES (?)", (senhaGerada,))
+conexao.commit()
 
 print("Senha gerada:", senhaGerada)
